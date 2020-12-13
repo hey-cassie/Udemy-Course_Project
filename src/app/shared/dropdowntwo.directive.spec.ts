703aae0ae3e70fd5c22cc135439e7d54762663ea
Cassie Jones <cassie@Cassies-MacBook-Pro.local>
2020-12-13 12:43:40 -0600

    section 8 complete

---
+++ src/app/shared/dropdowntwo.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DropdowntwoDirective } from './dropdowntwo.directive';
+
+describe('DropdowntwoDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DropdowntwoDirective();
+    expect(directive).toBeTruthy();
+  });
+});
