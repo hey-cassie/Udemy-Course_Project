c13d52ae71f9d7d0810c44f69fef1ee192d6c7e7
Cassie Jones <cassie@Cassies-MacBook-Pro.local>
2021-02-13 10:01:39 -0600

    section-20 finished

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 import { DataStorageService } from '../shared/data-storage.service';
 
 
@@ -8,12 +10,21 @@ import { DataStorageService } from '../shared/data-storage.service';
   templateUrl: './header.component.html',
   //styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  constructor(private dataStorageService: DataStorageService) {
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+  private userSub: Subscription;
+
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService) {
     const collapsed = true;
   }
 
   ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+      console.log(!user);
+      console.log(!!user);
+      // above is shorthand for >> this.isAuthenticated = !user ? false : true;
+    });
   }
 
   onSaveData() {
@@ -24,4 +35,11 @@ export class HeaderComponent implements OnInit {
     this.dataStorageService.fetchRecipes().subscribe();
   }
 
+  onLogout() {
+    this.authService.logout();
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { exhaustMap, map, take, tap } from 'rxjs/operators';
 
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipeService: RecipeService) {}
+    constructor(
+      private http: HttpClient, 
+      private recipeService: RecipeService, 
+      private authService: AuthService) {}
 
     storeRecipes() {
         const recipes = this.recipeService.getRecipes();
@@ -21,25 +25,25 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.http
+          return this.http
           .get<Recipe[]>(
-            'https://ng-course-recipe-book-65f10.firebaseio.com/recipes.json'
+            //'https://ng-course-recipe-book-65f10.firebaseio.com/recipes.json'
+            'https://udemy-course-project-afba6-default-rtdb.firebaseio.com/recipes.json'
           )
           .pipe(
             map(recipes => {
               return recipes.map(recipe => {
                 return {
-                  ...recipe,
-                  ingredients: recipe.ingredients ? recipe.ingredients : []
-                };
-              });
-            }),
-            tap(recipes => {
-              this.recipeService.setRecipes(recipes);
-            })
-            //tap operator allows us to access code without altering datd
-            //we no longer subcribe but return and in our header component we now need to subscribe
-          )
+                 ...recipe,
+                ingredients: recipe.ingredients ? recipe.ingredients : []
+              };
+            });
+          }),
+          tap(recipes => {
+            this.recipeService.setRecipes(recipes);
+          })
+        );
+        //tap operator allows us to access code without altering datd
+        //we no longer subcribe but return and in our header component we now need to subscribe);
       }
-
 }
